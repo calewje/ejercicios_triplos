986f4b824d79747d638fcddceb706b19b3d6b294
Karen_H <137447475+calewje@users.noreply.github.com>
2025-11-06 08:59:43 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//Ejercicios 
+
+//r = a +b -c *d;
+let a = 5; b= 4; c=3; d=1;
+let t1 = c*d;
+let t2 = a+b;
+let t3 = t2 - t1;
+let r = t3;
+console.log(r);
+
+//x = (m * n) / (p + q) -s;
+let m = 2; n= 4; p=3; q=5; s=4;
+let r1 = m * n;
+let r2 = p + q;
+let r3 = r1 / r2;
+let r4 = r3 - s;
+let x = r4;
+console.log(x);
+
+//total = (a + b + c)*(d - e);
+let a1 = 2; b1= 4; c1=3; d1=5; e=1;
+let s1 = a1 + b1;
+let s2 = s1 + c1;
+let s3 = d1 - e;
+let s4 = s2 * s3;
+let total = s4;
+console.log(total);
+
+// val = a * (b + c / d) -e;
+let a2 = 2; b2= 4; c2=3; d2=5; s1=1;
+let e1 = c2 + d2;
+let e2 = b2 + e1;
+let e3 = a2 - e2;
+let e4 = e3 * s1;
+let val = e4;
+console.log(val);
+
+// z = (x + y) * (a - b) / (c + d) 
+let a3 = 2; b3= 4; c3=3; d3=5; x1=1;y1=2;
+let p1 = x1 + y1;
+let p2 = a3 - b3;
+let p3 = c3 + d3;
+let p4 = p1 * p2;
+let p5 = p4 / p3;
+let z = p5;
+console.log(z);
+/* 
+    r = a +b -c *d;
+    x = (m * n) / (p + q) -r;
+    total = (a + b + c)*(d - e);
+    val = a * (b + c / d) -e;
+    z = (x + y) * (a - b) / (c + d) 
+
+*/
